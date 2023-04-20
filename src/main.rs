9597313c363b81f710c1a35dae2093715bbd84e8
Mattia Righetti <matt95.righetti@gmail.com>
2023-04-21 00:05:31 +0200

    Code refactor

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,57 @@
-use usvg::{fontdb, TreeParsing, TreeTextToPath};
+use std::collections::HashMap;
+use usvg::{
+    fontdb,
+    TreeParsing,
+    TreeTextToPath
+};
 
-fn main() {
+fn new(s: &String, args: Vec<String>) -> Result<String, liquid::Error> {
+    let args: HashMap<String, String> = args
+        .chunks_exact(2)
+        .map(|chunk| (chunk[0].clone(), chunk[1].clone()))
+        .collect();
+
+    let mut globals = liquid::model::Object::new();
+    for (k,v) in args {
+        let _ = globals
+            .insert(
+                k.into(),
+                liquid::model::to_value(&v)?
+            );
+    }
+
+    let template = liquid::ParserBuilder::with_stdlib()
+        .build()?
+        .parse(s)?;
+
+    template.render(&globals)
+}
+
+fn from_path(path: String, args: Vec<String>) -> Result<String, liquid::Error> {
+    let svg_string = std::fs::read_to_string(path).unwrap();
+    new(&svg_string, args)
+}
+
+fn main() -> Result<(), liquid::Error> {
     let mut args: Vec<String> = std::env::args().collect();
     if args.len() < 3 || args.len() & 1 == 0 {
         println!("Usage:\n\tminimal <in-svg> <out-png> (<liquid_key> <liquid_value>)*");
-        return;
+        return Ok(());
     }
 
     let _ = args.remove(0);
     let input_path = args.remove(0);
     let output_path = args.remove(0);
 
-    let mut opt = usvg::Options::default();
-    // Get file's absolute directory.
-    opt.resources_dir = std::fs::canonicalize(&input_path)
-        .ok()
-        .and_then(|p| p.parent().map(|p| p.to_path_buf()));
+    let svg = from_path(input_path, args)?;
+    let mut tree = usvg::Tree::from_data(
+        &svg.as_bytes(),
+        &usvg::Options::default()
+        )
+        .unwrap();
 
     let mut fontdb = fontdb::Database::new();
     fontdb.load_system_fonts();
-
-    let svg_data = std::fs::read_to_string(&input_path).unwrap();
-    
-    let template = liquid::ParserBuilder::with_stdlib()
-        .build().unwrap()
-        .parse(&svg_data).unwrap();
-
-    let mut globals = liquid::model::Object::new();
-
-    for pair in args.chunks(2).collect::<Vec<&[String]>>() {
-        println!("Injecting: {} -> {}", pair.get(1).unwrap(), pair.get(0).unwrap());
-        let _ = globals
-            .insert(
-                pair.get(0).unwrap().into(),
-                liquid::model::to_value(pair.get(1).unwrap()).unwrap()
-            );
-    }
-
-    let output = template.render(&globals).unwrap();
-    let mut tree = usvg::Tree::from_data(&output.as_bytes(), &opt).unwrap();
     tree.convert_text(&fontdb);
 
     let mut pixmap = tiny_skia::Pixmap::new(1920, 1080).unwrap();
@@ -49,5 +62,8 @@ fn main() {
         pixmap.as_mut(),
     )
     .unwrap();
+
     pixmap.save_png(output_path).unwrap();
+    
+    Ok(())
 }
